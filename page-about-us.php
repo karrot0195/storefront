06b9f630b5c2736ba315ab271ae3bc4772567aec
no <leno2796@gmail.com>
2019-07-05 13:24:52 +0700

    update

--- page-about-us.php
+++ page-about-us.php
@@ -14,15 +14,28 @@ get_header('home-1'); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <div class="sider">
+            <div class="slider">
                 <?php
-                    $slider = get_field('slider');
-                    var_dump($slider);
-                    $acttachmentUrl = isset($slider())
+                    $slider = get_field('slider_about_us');
+                   
+                    if($slider) :
                  ?>
                 <div class="slider__item">
+                    <?php
+                         $bg_image = $slider[0]['background_image'];
+                         echo '<pre>';
+                            print_r($bg_image);
+                         echo '</pre>';
+                         if($bg_image) :
+                         $attachmentUrl = isset($bg_image[0]['image']) ? wp_get_attachment_url($bg_image[0]['image']) : ''; 
+                         echo '<pre>';
+                            print_r($attachmentUrl);
+                         echo '</pre>';
+
+                    ?>
+
                     <div class="slider__item__img">
-                        <img class="bg-img">
+                        <img class="bg_img" alt="" src="<?= esc_url($attachmentUrl) ?>">
                     </div>
                     <div class="slider__item__content">
                         <div class="title">
@@ -31,7 +44,9 @@ get_header('home-1'); ?>
                         <div class="sub-title">
                         </div>
                     </div>
+                    <?php endif ?>
                 </div>
+                    <?php endif ?>
             <div>
 		
 		</main><!-- #main -->
