33a6ff7006fd68c14effc9acee24830334735f20
thanhhieu0195 <thanhhieu0195@gmail.com>
2019-07-13 00:31:28 +0700

    add bookmark

--- helpers/Helper.php
+++ helpers/Helper.php
@@ -7,3 +7,32 @@ function render_php($path,$args=[]){
     ob_end_clean();
     return $var;
 }
+
+
+function login($user_id) {
+	wp_clear_auth_cookie();
+    wp_set_current_user ( $user_id );
+    wp_set_auth_cookie( $user_id );
+}
+
+function get_user_bookmark() {
+	$bookmark = [];
+	if (is_user_logged_in()) {
+		$user_id = get_current_user_id();
+		$bookmark = get_user_meta($user_id, 'bookmark', true);
+	    if (empty($bookmark)) {
+	    	$bookmark = [];
+	    }
+	}
+    return $bookmark;
+}
+
+function get_class_the_bookmark() {
+	$post_id = get_the_ID();
+	$bookmark = get_user_bookmark();
+	if (in_array($post_id, $bookmark)) {
+		return 'bookmark';
+	}
+
+	return '';
+}
