739bfa37029494d15ce243b0f3a0ba85fbf9bb67
tai_techvsi <tai@techvsi.com>
2019-07-14 02:52:31 +0700

    update css

---
+++ page-wishlist.php
@@ -0,0 +1,289 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package storefront
+ */
+
+get_header('home-1'); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main page-wishlist" role="main">
+            <ul class="products columns-4">
+                <div class="loop-main-product">
+                    <li class="wrap-product-item product type-product">
+                        <i class="ion ion-md-close-circle icon-close"></i>
+                        <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                            <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
+                            <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
+                            <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
+                        </a>
+                        <div class="wrap-cart">
+                            <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
+                                <div class="wrap-block wrap-init">
+                                <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                                
+                                </div>
+                                <div class="wrap-block wrap-proccess">
+                                    <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
+                                        <i class="icon ion-md-remove"></i> 
+                                    </div>
+                                    <div class="block-number">
+                                        <div id="number"><div class="val">1</div></div>
+                                    </div>
+                                    <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
+                                        <i class="icon ion-md-add"></i>  
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="stock">
+                            <div class="wrapper">
+                                <span><i class="ion ion-ios-information-circle"></i></span>
+                                <span class="text">Low in stock</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="wrap-product-item product type-product">
+                        <i class="ion ion-md-close-circle icon-close"></i>
+                        <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                            <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
+                            <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
+                            <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
+                        </a>
+                        <div class="wrap-cart">
+                            <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
+                                <div class="wrap-block wrap-init">
+                                <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                                
+                                </div>
+                                <div class="wrap-block wrap-proccess">
+                                    <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
+                                        <i class="icon ion-md-remove"></i> 
+                                    </div>
+                                    <div class="block-number">
+                                        <div id="number"><div class="val">1</div></div>
+                                    </div>
+                                    <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
+                                        <i class="icon ion-md-add"></i>  
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="stock">
+                            <div class="wrapper">
+                                <span><i class="ion ion-ios-information-circle"></i></span>
+                                <span class="text">Low in stock</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="wrap-product-item product type-product">
+                        <i class="ion ion-md-close-circle icon-close"></i>
+                        <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                            <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
+                            <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
+                            <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
+                        </a>
+                        <div class="wrap-cart">
+                            <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
+                                <div class="wrap-block wrap-init">
+                                <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                                
+                                </div>
+                                <div class="wrap-block wrap-proccess">
+                                    <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
+                                        <i class="icon ion-md-remove"></i> 
+                                    </div>
+                                    <div class="block-number">
+                                        <div id="number"><div class="val">1</div></div>
+                                    </div>
+                                    <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
+                                        <i class="icon ion-md-add"></i>  
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="stock">
+                            <div class="wrapper">
+                                <span><i class="ion ion-ios-information-circle"></i></span>
+                                <span class="text">Low in stock</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="wrap-product-item product type-product">
+                        <i class="ion ion-md-close-circle icon-close"></i>
+                        <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                            <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
+                            <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
+                            <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
+                        </a>
+                        <div class="wrap-cart">
+                            <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
+                                <div class="wrap-block wrap-init">
+                                <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                                
+                                </div>
+                                <div class="wrap-block wrap-proccess">
+                                    <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
+                                        <i class="icon ion-md-remove"></i> 
+                                    </div>
+                                    <div class="block-number">
+                                        <div id="number"><div class="val">1</div></div>
+                                    </div>
+                                    <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
+                                        <i class="icon ion-md-add"></i>  
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="stock">
+                            <div class="wrapper">
+                                <span><i class="ion ion-ios-information-circle"></i></span>
+                                <span class="text">Low in stock</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="wrap-product-item product type-product">
+                        <i class="ion ion-md-close-circle icon-close"></i>
+                        <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                            <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
+                            <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
+                            <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
+                        </a>
+                        <div class="wrap-cart">
+                            <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
+                                <div class="wrap-block wrap-init">
+                                <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                                
+                                </div>
+                                <div class="wrap-block wrap-proccess">
+                                    <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
+                                        <i class="icon ion-md-remove"></i> 
+                                    </div>
+                                    <div class="block-number">
+                                        <div id="number"><div class="val">1</div></div>
+                                    </div>
+                                    <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
+                                        <i class="icon ion-md-add"></i>  
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="stock">
+                            <div class="wrapper">
+                                <span><i class="ion ion-ios-information-circle"></i></span>
+                                <span class="text">Low in stock</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="wrap-product-item product type-product">
+                        <i class="ion ion-md-close-circle icon-close"></i>
+                        <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                            <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
+                            <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
+                            <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
+                        </a>
+                        <div class="wrap-cart">
+                            <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
+                                <div class="wrap-block wrap-init">
+                                <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                                
+                                </div>
+                                <div class="wrap-block wrap-proccess">
+                                    <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
+                                        <i class="icon ion-md-remove"></i> 
+                                    </div>
+                                    <div class="block-number">
+                                        <div id="number"><div class="val">1</div></div>
+                                    </div>
+                                    <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
+                                        <i class="icon ion-md-add"></i>  
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="stock">
+                            <div class="wrapper">
+                                <span><i class="ion ion-ios-information-circle"></i></span>
+                                <span class="text">Low in stock</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="wrap-product-item product type-product">
+                        <i class="ion ion-md-close-circle icon-close"></i>
+                        <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                            <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
+                            <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
+                            <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
+                        </a>
+                        <div class="wrap-cart">
+                            <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
+                                <div class="wrap-block wrap-init">
+                                <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                                
+                                </div>
+                                <div class="wrap-block wrap-proccess">
+                                    <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
+                                        <i class="icon ion-md-remove"></i> 
+                                    </div>
+                                    <div class="block-number">
+                                        <div id="number"><div class="val">1</div></div>
+                                    </div>
+                                    <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
+                                        <i class="icon ion-md-add"></i>  
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="stock">
+                            <div class="wrapper">
+                                <span><i class="ion ion-ios-information-circle"></i></span>
+                                <span class="text">Low in stock</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="wrap-product-item product type-product">
+                        <i class="ion ion-md-close-circle icon-close"></i>
+                        <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                            <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
+                            <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
+                            <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
+                        </a>
+                        <div class="wrap-cart">
+                            <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
+                                <div class="wrap-block wrap-init">
+                                <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                                
+                                </div>
+                                <div class="wrap-block wrap-proccess">
+                                    <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
+                                        <i class="icon ion-md-remove"></i> 
+                                    </div>
+                                    <div class="block-number">
+                                        <div id="number"><div class="val">1</div></div>
+                                    </div>
+                                    <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
+                                        <i class="icon ion-md-add"></i>  
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="stock">
+                            <div class="wrapper">
+                                <span><i class="ion ion-ios-information-circle"></i></span>
+                                <span class="text">Low in stock</span>
+                            </div>
+                        </div>
+                    </li>
+                </div>
+            </ul>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer('home-1');
