30635f91b1d60b40b8f1a38cd25eefa5aba85f73
thanhhieu0195 <thanhhieu0195@gmail.com>
2019-07-14 12:38:54 +0700

    Add relate

--- cart/cart.php
+++ cart/cart.php
@@ -16,7 +16,9 @@
  */
 
 defined( 'ABSPATH' ) || exit;
-do_action( 'woocommerce_before_cart' ); ?>
+do_action( 'woocommerce_before_cart' );
+$product_ids = [];
+?>
 <div class="container">
 	<form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
 		<?php do_action( 'woocommerce_before_cart_table' ); ?>
@@ -40,7 +42,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 				foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 					$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
 					$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
-
+                    $product_ids[] = $product_id;
 					if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
 						$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
 						?>
@@ -170,7 +172,13 @@ do_action( 'woocommerce_before_cart' ); ?>
 		?>
 	</div>
 
-	<?=  render_php('views/cart/relate-product.php') ?>
+	<?php
+    echo render_php('views/cart/relate-product.php', [
+        'title' => get_field('title'),
+        'description' => get_field('description'),
+        'relate_product' => $product_ids
+    ]);
+    ?>
 </div>
 
 <?php do_action( 'woocommerce_after_cart' ); ?>
--- views/cart/relate-product.php
+++ views/cart/relate-product.php
@@ -1,122 +1,84 @@
+<?php
+if (!isset($title)) {
+    $title = '';
+}
+
+if (!isset($description)) {
+    $description = '';
+}
+if (!isset($relate_product)) {
+    $relate_product = [];
+}
+
+$term_ids = [];
+if (!empty($relate_product)) {
+    for ($i=0; $i<count($relate_product); $i++) {
+        $terms = wp_get_post_terms($relate_product[$i], 'product_cat', ['field' => ['term_id']]);
+        $ids = array_column($terms, 'slug');
+        $term_ids = array_merge($term_ids, $ids);
+    }
+}
+
+$query = new WC_Product_Query( [
+    'category' => $term_ids,
+    'exclude' => $relate_product
+] );
+
+$products = $query->get_products();
+?>
 <div class="related">
     <div class="container">
         <div class="related-wrapper">
             <div class="content-related">
-                <div class="title"><?= esc_html__('Related Product','storefront') ?></div>
+                <div class="title"><?= $title ?></div>
                 <div class="description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla gravida arcu sit amet ante convallis, et venenatis sapien lacinia. Sed porta feugiat egestas. Proin porttitor ligula porta fringilla rutrum.
+                    <?= $description ?>
                 </div>
             </div>
             <div class="image-related">
                 <ul class="products columns-4">
                     <div class="loop-main-product related-slider">
-                        <li class="wrap-product-item product type-product">
-                            <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-                                <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
-                                <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
-                                <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
-                            </a>
-                            <div class="wrap-cart">
-                                <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
-                                    <div class="wrap-block wrap-init">
-                                        <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                        <?php
+                        if (!empty($products)) {
+                            foreach ($products as $product) {
+                                $thumbnail_url = get_the_post_thumbnail_url($product->get_id());
 
-                                    </div>
-                                    <div class="wrap-block wrap-proccess">
-                                        <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
-                                            <i class="icon ion-md-remove"></i>
-                                        </div>
-                                        <div class="block-number">
-                                            <div id="number"><div class="val">1</div></div>
-                                        </div>
-                                        <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
-                                            <i class="icon ion-md-add"></i>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
-                        <li class="wrap-product-item product type-product">
-                            <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-                                <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
-                                <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
-                                <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
-                            </a>
-                            <div class="wrap-cart">
-                                <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
-                                    <div class="wrap-block wrap-init">
-                                        <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                                ?>
+                                <li class="wrap-product-item product type-product">
+                                    <a href="<?= $product->get_permalink() ?>" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                                        <img src="<?= esc_url($thumbnail_url) ?>" alt="#">
+                                        <h2 class="woocommerce-loop-product__title"><?= $product->get_title() ?></h2>
+                                        <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
+                                    </a>
+                                    <div class="wrap-cart">
+                                        <form class="increse-decrease fm-sl-cart proccessing" data-product_id="<?= $product->get_id() ?>">
+                                            <div class="wrap-block wrap-init">
+                                                <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value"><?= esc_html__('Add to cart', 'storefront') ?></div>
 
+                                            </div>
+                                            <div class="wrap-block wrap-proccess">
+                                                <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
+                                                    <i class="icon ion-md-remove"></i>
+                                                </div>
+                                                <div class="block-number">
+                                                    <div id="number"><div class="val">1</div></div>
+                                                </div>
+                                                <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
+                                                    <i class="icon ion-md-add"></i>
+                                                </div>
+                                            </div>
+                                        </form>
                                     </div>
-                                    <div class="wrap-block wrap-proccess">
-                                        <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
-                                            <i class="icon ion-md-remove"></i>
-                                        </div>
-                                        <div class="block-number">
-                                            <div id="number"><div class="val">1</div></div>
-                                        </div>
-                                        <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
-                                            <i class="icon ion-md-add"></i>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
-                        <li class="wrap-product-item product type-product">
-                            <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-                                <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
-                                <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
-                                <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
-                            </a>
-                            <div class="wrap-cart">
-                                <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
-                                    <div class="wrap-block wrap-init">
-                                        <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
+                                </li>
+                                <?php
+                            }
+                        }
+                        ?>
 
-                                    </div>
-                                    <div class="wrap-block wrap-proccess">
-                                        <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
-                                            <i class="icon ion-md-remove"></i>
-                                        </div>
-                                        <div class="block-number">
-                                            <div id="number"><div class="val">1</div></div>
-                                        </div>
-                                        <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
-                                            <i class="icon ion-md-add"></i>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
-                        <li class="wrap-product-item product type-product">
-                            <a href="#" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-                                <img src="http://stg1.techvsi.com/wp-content/uploads/2019/06/3.png" alt="#">
-                                <h2 class="woocommerce-loop-product__title">Chira Wash</h2>
-                                <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">SGD</span>45</span></span>
-                            </a>
-                            <div class="wrap-cart">
-                                <form class="increse-decrease fm-sl-cart proccessing" data-product_id="141">
-                                    <div class="wrap-block wrap-init">
-                                        <div class="lb-btn js-cart-btn" data-val="1" id="increase" value="Increase Value">Add to cart</div>
-
-                                    </div>
-                                    <div class="wrap-block wrap-proccess">
-                                        <div class="value-button js-cart-btn" data-val="-1" id="decrease" value="Decrease Value">
-                                            <i class="icon ion-md-remove"></i>
-                                        </div>
-                                        <div class="block-number">
-                                            <div id="number"><div class="val">1</div></div>
-                                        </div>
-                                        <div class="value-button js-cart-btn" data-val="1" id="increase" value="Increase Value">
-                                            <i class="icon ion-md-add"></i>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
                     </div>
                 </ul>
             </div>
         </div>
     </div>
 </div>
+}
