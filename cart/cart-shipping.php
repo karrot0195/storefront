03d30c05bec98c5c023ddecc0d41088e66ceebbc
thanhhieu0195 <thanhhieu0195@gmail.com>
2019-07-19 00:49:15 +0700

    Add code

--- cart/cart-shipping.php
+++ cart/cart-shipping.php
@@ -37,7 +37,12 @@ $calculator_text          = '';
 						} else {
 							printf( '<input type="hidden" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d_%2$s" value="%3$s" class="shipping_method" />', $index, esc_attr( sanitize_title( $method->id ) ), esc_attr( $method->id ) ); // WPCS: XSS ok.
 						}
-						printf( '<label for="shipping_method_%1$s_%2$s">%3$s</label>', $index, esc_attr( sanitize_title( $method->id ) ), wc_cart_totals_shipping_method_label( $method ) ); // WPCS: XSS ok.
+
+						$text = '';
+						if (!empty($method) ) {
+						    $text = '<span class="label">'.$method->get_label().'</span>'.'<span class="cost">'.wc_price($method->get_cost()).'</span>';
+                        }
+						printf( '<label for="shipping_method_%1$s_%2$s">%3$s</label>', $index, esc_attr( sanitize_title( $method->id ) ), $text ); // WPCS: XSS ok.
 						do_action( 'woocommerce_after_shipping_rate', $method, $index );
 						?>
 					</li>
