94e4e749f1a7705e2b033cb37a6ee6713a2278bb
no <leno2796@gmail.com>
2019-07-20 15:33:01 +0700

    update

--- page-templates/return-policy.php
+++ page-templates/return-policy.php
@@ -9,44 +9,45 @@ get_header('home-1'); ?>
 		<main id="main" class="site-main return-policy privacy-policy" role="main">
             <div class="container">
                 <div class="privacy-policy-wrapper return-policy-wrapper">
-                    <?php 
-                        $return_policy = get_field('return_policy') ;
-                        if($return_policy) :
-                            $side_bar = $return_policy[0]['sidebar'];
-                    ?>
                     <div class="sidebar">
-                       <p> <?php echo $side_bar ?></p>
+                        <?php echo get_field('return_policy_sidebar') ?>
                     </div>    
                     <div class="content">
-                        <?php 
-                            $content=$return_policy[0]['content'];
-                           
-                        ?>
                         <div class="desc">
                             <?php echo get_field('description') ?>
                         </div>
                         <?php 
-                            $content=$return_policy[0]['content'];
+                            $content=get_field('return_policy_content');
                             if($content) :
-                                foreach($content as $idx_block_content) :
+                                foreach($content as $content_item) :
                                    
                         ?>
                             <div class="block__content">
-                                <?php  $block_content = $idx_block_content['block_content']; ?>
                                 <div class="title">
-                                    <?php echo $block_content[0]['title'] ?>
+                                    <?php echo $content_item['return_policy_category'] ?>
                                 </div>
                                 <div class="sub-title">
-                                <?php echo $block_content[0]['sub_title'] ?>
+                                    <?php echo $content_item['answer'] ?>
                                 </div>
-                                <div class="extra-title">
-                                    <?php echo $block_content[0]['extra_title'] ?>
-                                </div>
+                                <?php
+                                    $extra_block = $content_item['extra_block'];
+                                        if($extra_block) :
+                                            foreach($extra_block as $extra_block_item) :
+                                ?>
+                                        <div class="extra-block">
+                                            <div class="extra-title">
+                                                <?php echo $extra_block_item['title'] ?>
+                                            </div>
+                                            <div class="extra-desc">
+                                                <?php echo $extra_block_item['description'] ?>
+                                            </div>
+                                        </div>
+                                    <?php endforeach ?>
+                                <?php endif ?>
                             </div>
                             <?php endforeach ?>
                         <?php endif ?>
                     </div>
-                    <?php endif ?>
                 </div>        
             </div>
 		</main><!-- #main -->
