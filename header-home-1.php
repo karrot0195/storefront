32c5c952350bef45401a2f16ff057f7038dacad4
no <leno2796@gmail.com>
2019-07-24 15:53:23 +0700

    update

--- header-home-1.php
+++ header-home-1.php
@@ -42,6 +42,7 @@ if (is_page_template('templates/template-home1.php')) {
     $action = 'storefront_header_home_1';
 }
 ?>
+
 <div id="page" class="hfeed site home-page-1 <?= implode(' ', $class) ?>">
     <header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
         <div class="container">
