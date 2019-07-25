3b865a89761bb16ba74cab0b3f81ef5a183d0516
thanhhieu0195 <thanhhieu0195@gmail.com>
2019-07-25 20:27:12 +0700

    add code

--- cart/cart.php
+++ cart/cart.php
@@ -41,13 +41,16 @@ $product_ids = [];
 				<?php
 				foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 					$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+					
+					$attributes = $_product->get_attributes();
+					
 					$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
                     $product_ids[] = $product_id;
+
 					if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
 						$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
 						?>
 						<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
-
 							<td class="product-remove">
 								<?php
 									// @codingStandardsIgnoreLine
@@ -93,7 +96,10 @@ $product_ids = [];
 
 							<td>
 								<div class="wrap-size">
-									<span><?= $_product->get_attribute('pa_size') ?></span>
+									<select class="sl-attr-size">
+										<option>25 ml</option>
+										<option>35 ml</option>
+									</select>
 								</div>
 							</td>
 
