63cfa4725971d1e5bcee377df3fdf4b84177704b
Thanh Hieu <thanhhieu0195@gmail.com>
2019-07-25 11:55:27 +0700

    add code

--- helpers/Ajax.php
+++ helpers/Ajax.php
@@ -227,10 +227,37 @@ function action_create_account() {
     $password = $_POST['password'];
     $user = wp_create_user( $username, $password, $email);
     if( is_wp_error( $user ) ) {
-        $result['message'] = $user->get_error_message();
-    } else {
+        $result['message'] = $user->get_error_message()
+;    } else {
         login($user);
         $result['error'] = false;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($result);
+    exit();
+}
+
+add_action('wp_ajax_action_forgot_password', 'action_forgot_password');
+add_action('wp_ajax_nopriv_action_forgot_password', 'action_forgot_password');
+
+function action_forgot_password() {
+    $result = [
+        'error' => true,
+        'message' => ''
+    ];
+    $email = $_POST['email'];
+    $user = get_user_by_email($email);
+    if ($user) {
+        $pass = generateRandomString();
+        $to = $email;
+        $subject = 'DERMA SYSTEM';
+        $body = 'Password current: '. $pass;
+        $headers = array('Content-Type: text/html; charset=UTF-8');
+        wp_mail( $to, $subject, $body, $headers );
+        wp_set_password($pass, $user->ID);
+        $result['error'] = false;
+    } else {
+        $result['message'] = 'User is not exists';
     }
     header('Content-Type: application/json');
     echo json_encode($result);
