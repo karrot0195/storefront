80ca0464d095b2a6dc7c2996f939e5682bec9e4a
no <leno2796@gmail.com>
2019-07-27 04:04:18 +0700

    updae

--- checkout/thankyou.php
+++ checkout/thankyou.php
@@ -82,5 +82,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Thank you. Your order has been received.', 'woocommerce' ), null ); ?></p>
 
 	<?php endif; ?>
+	<?php
+	echo render_php('views/common/back-to-shopping.php');
+   
+    ?>
 
 </div>
