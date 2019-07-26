750132c8f72443899106cc32f96d5d893537dc31
no <leno2796@gmail.com>
2019-07-27 01:06:43 +0700

    updae

--- order/order-details-item.php
+++ order/order-details-item.php
@@ -32,7 +32,7 @@ $thumbnail_url = get_the_post_thumbnail_url($item->get_product_id());
 			$product_permalink = apply_filters( 'woocommerce_order_item_permalink', $is_visible ? $product->get_permalink( $item ) : '', $item, $order );
 
 			echo apply_filters( 'woocommerce_order_item_name', $product_permalink ? sprintf( '<a href="%s">%s</a>', $product_permalink, $item->get_name() ) : $item->get_name(), $item, $is_visible );
-			echo apply_filters( 'woocommerce_order_item_quantity_html', ' <strong class="product-quantity">' . sprintf( '&times; %s', $item->get_quantity() ) . '</strong>', $item );
+			echo apply_filters( 'woocommerce_order_item_quantity_html', ' <span class="product-quantity">' . sprintf( '&times; %s', $item->get_quantity() ) . '</span>', $item );
 
 			do_action( 'woocommerce_order_item_meta_start', $item_id, $item, $order, false );
 
